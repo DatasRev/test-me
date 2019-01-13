a01248a9926738047f294c7d011e7833f6872888
Levente Laszlo <leventewww@gmail.com>
2019-01-13 19:46:47 +0100

    Init

---
+++ base.py
@@ -0,0 +1,6 @@
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+new_list = [ elem ** 2 for elem in my_list ]
+
+print("Basic operations...")
+print(new_list)
---
+++ numeric.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+my_list = [1, 2, 3, 4, 5, 6]
+np_arr = np.array(my_list)
+
+print("convert a list into numpy array")
+print(np_arr)
